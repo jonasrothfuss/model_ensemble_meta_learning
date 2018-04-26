5bf72ed3e38784c8a44b65545f2accf59e2a9eac
Jonas Rothfuss <jonas.rothfuss@gmx.de>
2018-04-25 17:14:37 -0700

    adding AWS regions to setup

--- scripts/setup_ec2_for_rllab.py
+++ scripts/setup_ec2_for_rllab.py
@@ -228,7 +228,9 @@ def setup_s3():
 
 
 def setup_ec2():
-    for region in ["us-east-1", "us-west-1", "us-west-2"]:
+    for region in ['us-east-1', 'us-west-1', 'us-west-2', 'us-east-2', 'ap-northeast-1',
+                   'ap-northeast-2', 'ap-south-1', 'ap-southeast-1', 'ap-southeast-2', 'ca-central-1',
+                   'eu-central-1', 'eu-west-1', 'eu-west-2', 'eu-west-3', 'sa-east-1']:
         print("Setting up region %s" % region)
 
         ec2 = boto3.resource(
