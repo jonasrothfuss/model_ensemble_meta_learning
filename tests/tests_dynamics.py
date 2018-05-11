68508e073878e93d388787af6d8e4a7b321c5231
Jonas Rothfuss <jonas.rothfuss@gmx.de>
2018-05-10 20:26:13 -0700

    improve dynamic model test cases

--- tests/tests_dynamics.py
+++ tests/tests_dynamics.py
@@ -45,22 +45,56 @@ class TestMLPDynamics(unittest.TestCase):
 
         self.assertAlmostEquals(diff, 0, places=2)
 
-    def test_train_prediction(self):
-        # just checks if training and prediction runs without errors and prediction returns correct shapes
+
+    def test_train_prediction1(self):
         env = PointEnv()
-        paths = sample_random_trajectories_point_env(env, num_paths=10, horizon=10)
-        dynamics_model = MLPDynamicsModel("dyn_model_2", env, hidden_sizes=(16, 16))
+        obs = np.random.uniform(-2, 2, size=(20000, 2))
+        act = np.random.uniform(-0.1, 0.1, size=(20000, 2))
+        next_obs = obs + act
+
+        dynamics_model = MLPDynamicsModel("dyn_model_2a", env, hidden_sizes=(32, 32), normalize_input=False)
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+
+            dynamics_model.fit(obs, act, next_obs, epochs=10, verbose=True)
+
+            obs_test = np.random.uniform(-2, 2, size=(20000, 2))
+            act_test = np.random.uniform(-0.1, 0.1, size=(20000, 2))
+            obs_next_test = obs_test + act_test
+
+            obs_next_pred = dynamics_model.predict(obs_test, act_test)
+            mean_diff = np.mean(np.abs(obs_next_test - obs_next_pred))
+            print("Mean Diff:", mean_diff)
+
+            self.assertEqual(obs_next_pred.shape, obs_test.shape)
+            self.assertLessEqual(mean_diff, 0.01)
+
+    def test_train_prediction2(self):
+        # just checks if training and prediction runs without errors and prediction returns correct shapes
+
+        env = PointEnv()
+        paths = sample_random_trajectories_point_env(env, num_paths=500, horizon=100)
+        dynamics_model = MLPDynamicsModel("dyn_model_2b", env, hidden_sizes=(32, 32), normalize_input=False)
 
         obs = np.concatenate([path['observations'] for path in paths], axis=0)
         obs_next = np.concatenate([path['next_observations'] for path in paths], axis=0)
         act = np.concatenate([path['actions'] for path in paths], axis=0)
 
+        obs_test = np.random.uniform(-2, 2, size=(20000, 2))
+        act_test = np.random.uniform(-0.1, 0.1, size=(20000, 2))
+        obs_next_test = obs_test + act_test
+
         with tf.Session() as sess:
             sess.run(tf.global_variables_initializer())
 
-            dynamics_model.fit(obs, act, obs_next, epochs=5)
-            obs_pred = dynamics_model.predict(obs, act)
-            self.assertEqual(obs_pred.shape, obs.shape)
+            dynamics_model.fit(obs, act, obs_next, epochs=20, verbose=True)
+            obs_next_pred = dynamics_model.predict(obs_test, act_test)
+
+            mean_diff = np.mean(np.abs(obs_next_test - obs_next_pred))
+            print("Mean Diff:", mean_diff)
+
+            self.assertEqual(obs_next_pred.shape, obs_test.shape)
+            self.assertLessEqual(mean_diff, 0.01)
 
     def test_train_prediction_performance(self):
         # just checks if training and prediction runs without errors and prediction returns correct shapes
@@ -120,31 +154,41 @@ class TestMLPDynamicsEnsemble(unittest.TestCase):
     def test_train_prediction(self):
         # just checks if training and prediction runs without errors and prediction returns correct shapes
         env = PointEnv()
-        paths = sample_random_trajectories_point_env(env, num_paths=10, horizon=10)
+        paths = sample_random_trajectories_point_env(env, num_paths=200, horizon=100)
         dynamics_model = MLPDynamicsEnsemble("dyn_ensemble_2", env, hidden_sizes=(16, 16), num_models=5)
 
         obs = np.concatenate([path['observations'] for path in paths], axis=0)
         obs_next = np.concatenate([path['next_observations'] for path in paths], axis=0)
         act = np.concatenate([path['actions'] for path in paths], axis=0)
 
+        paths_test = sample_random_trajectories_point_env(env, num_paths=10, horizon=100)
+        obs_test = np.concatenate([path['observations'] for path in paths_test], axis=0)
+        obs_next_test = np.concatenate([path['next_observations'] for path in paths_test], axis=0)
+        act_test = np.concatenate([path['actions'] for path in paths_test], axis=0)
+
         with tf.Session() as sess:
             sess.run(tf.global_variables_initializer())
 
-            dynamics_model.fit(obs, act, obs_next, epochs=5)
-            obs_pred = dynamics_model.predict(obs, act, pred_type='mean')
-            self.assertEqual(obs_pred.shape, obs.shape)
+            dynamics_model.fit(obs, act, obs_next, epochs=10)
 
-            obs_pred = dynamics_model.predict(obs, act, pred_type='rand')
-            self.assertEqual(obs_pred.shape, obs.shape)
+            obs_pred1 = dynamics_model.predict(obs_test, act_test, pred_type='mean')
+            diff1 = np.mean(np.abs(obs_pred1 - obs_next_test) ** 2)
+            self.assertEqual(obs_pred1.shape, obs.shape)
+            self.assertLess(diff1, 0.01)
 
-            obs_pred = dynamics_model.predict(obs, act, pred_type='all')
-            self.assertEqual(obs_pred.shape, obs.shape + (5,))
+            obs_pred2 = dynamics_model.predict(obs_test, act_test, pred_type='rand')
+            diff2 = np.mean(np.abs(obs_pred2 - obs_next_test) ** 2)
+            self.assertEqual(obs_pred2.shape, obs.shape)
+            self.assertLess(diff2, 0.01)
+
+            obs_pred3 = dynamics_model.predict(obs_test, act_test, pred_type='all')
+            self.assertEqual(obs_pred3.shape, obs.shape + (5,))
 
     def test_train_prediction_std(self):
         # just checks if std prediction returns correct shapes
         env = PointEnv()
         paths = sample_random_trajectories_point_env(env, num_paths=10, horizon=10)
-        dynamics_model = MLPDynamicsEnsemble("dyn_ensemble_2", env, hidden_sizes=(16, 16), num_models=5)
+        dynamics_model = MLPDynamicsEnsemble("dyn_ensemble_3", env, hidden_sizes=(16, 16), num_models=5)
 
         obs = np.concatenate([path['observations'] for path in paths], axis=0)
         obs_next = np.concatenate([path['next_observations'] for path in paths], axis=0)
