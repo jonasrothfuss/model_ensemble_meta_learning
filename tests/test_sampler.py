be4b8e84a3e718a6f5852556cf3c8041f633573f
Jonas Rothfuss <jonas.rothfuss@gmx.de>
2018-05-11 17:00:31 -0700

    test case for random sampling

--- tests/test_sampler.py
+++ tests/test_sampler.py
@@ -18,6 +18,7 @@ from sandbox.rocky.tf.algos.trpo import TRPO
 
 from sandbox.jonas.sampler.model_vec_env_executor import ModelVecEnvExecutor
 from sandbox.jonas.sampler.model_vectorized_sampler import ModelVectorizedSampler
+from sandbox.jonas.sampler.random_vectorized_sampler import RandomVectorizedSampler
 
 import tensorflow as tf
 
@@ -32,7 +33,7 @@ def sample_random_trajectories_point_env(env, num_paths=100, horizon=100):
 
 class TestModelSampler(unittest.TestCase):
 
-    def test_sampling(self):
+    def test_policy_sampling(self):
         # get from data
         env = PointEnv()
         paths = sample_random_trajectories_point_env(env, num_paths=100, horizon=100)
@@ -81,5 +82,54 @@ class TestModelSampler(unittest.TestCase):
 
             print(samples_data.keys())
 
+    def test_random_sampling(self):
+        # get from data
+        env = PointEnv()
+        paths = sample_random_trajectories_point_env(env, num_paths=100, horizon=100)
+        dynamics_model = MLPDynamicsModel("dyn_model", env, hidden_sizes=(16,16))
+
+        obs = np.concatenate([path['observations'] for path in paths], axis=0)
+        obs_next = np.concatenate([path['next_observations'] for path in paths], axis=0)
+        act = np.concatenate([path['actions'] for path in paths], axis=0)
+
+        env = TfEnv(normalize(PointEnv()))
+
+        policy = GaussianMLPPolicy(
+            name="policy",
+            env_spec=env.spec,
+            hidden_sizes=(16, 16),
+            hidden_nonlinearity=tf.nn.tanh
+        )
+
+        baseline = LinearFeatureBaseline(env_spec=env.spec)
+
+        # fit dynamics model
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+
+            dynamics_model.fit(obs, act, obs_next, epochs=5)
+
+            algo = TRPO(
+                env=env,
+                policy=policy,
+                baseline=baseline,
+                batch_size=20000,
+                max_path_length=100,
+                n_itr=10,
+                discount=0.99,
+                step_size=0.01,
+            )
+
+            algo.dynamics_model = dynamics_model
+
+            itr = 1
+
+            random_sampler = RandomVectorizedSampler(algo)
+            random_sampler.start_worker()
+            paths = random_sampler.obtain_samples(itr)
+            samples_data = random_sampler.process_samples(itr, paths)
+
+            self.assertTrue(set(samples_data.keys()) ==
+                            set(['actions_dynamics', 'next_observations_dynamics', 'observations_dynamics']))
 if __name__ == '__main__':
     unittest.main()
