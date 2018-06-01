9a616f6652eea370e49850acdce76cebf4a1ecf7
Ignasi <ignasi78@gmail.com>
2018-06-01 11:12:02 -0700

    New AntEnv

--- rllab/envs/mujoco/ant_env.py
+++ rllab/envs/mujoco/ant_env.py
@@ -22,7 +22,6 @@ class AntEnv(MujocoEnv, Serializable):
         return np.concatenate([
             self.model.data.qpos.flat,
             self.model.data.qvel.flat,
-            np.clip(self.model.data.cfrc_ext, -1, 1).flat,
             self.get_body_xmat("torso").flat,
             self.get_body_com("torso"),
         ]).reshape(-1)
