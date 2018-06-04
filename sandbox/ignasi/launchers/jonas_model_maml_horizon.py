dfe1a6b0587cff45b2cdc0b45db520a333c42aaa
Ignasi <ignasi78@gmail.com>
2018-06-04 11:07:29 -0700

    jonas hyperparams

--- sandbox/ignasi/launchers/jonas_model_maml_horizon.py
+++ sandbox/ignasi/launchers/jonas_model_maml_horizon.py
@@ -105,20 +105,20 @@ def run_experiment(argv):
     vg.add('path_length_env', [1000])
 
     # Model-based MAML algo spec
-    vg.add('path_length_dyn', [500, 1000])
-    vg.add('n_itr', [50])
+    vg.add('path_length_dyn', [500])
+    vg.add('n_itr', [60])
     vg.add('fast_lr', [0.01])
     vg.add('meta_step_size', [0.01])
     vg.add('meta_batch_size', [20]) # must be a multiple of num_models
-    vg.add('discount', [0.95])
-    vg.add('batch_size_env_samples', [5])
-    vg.add('batch_size_dynamics_samples', [20])
+    vg.add('discount', [0.99])
+    vg.add('batch_size_env_samples', [1])
+    vg.add('batch_size_dynamics_samples', [50])
     vg.add('initial_random_samples', [5000])
     vg.add('dynamic_model_epochs', [(100, 50)])
     vg.add('num_maml_steps_per_iter', [30])
     vg.add('retrain_model_when_reward_decreases', [False])
     vg.add('reset_from_env_traj', [False, True])
-    vg.add('num_models', [10])
+    vg.add('num_models', [5])
     vg.add('trainable_step_size', [False])
 
     # neural network configuration
@@ -134,7 +134,7 @@ def run_experiment(argv):
     vg.add('dynamics_model', ['MLPDynamicsEnsemble', 'MLPProbabilisticDynamicsEnsemble'])
     vg.add('bias_transform', [False])
     vg.add('param_noise_std', [0.0])
-    # vg.add('nm_mbs_envs', [(5, 10, 2), (10, 10, 2), (10, 20, 1), (20, 20, 1)])
+    vg.add('nm_mbs_envs', [(5, 10, 2), (10, 10, 2), (10, 20, 1), (20, 20, 1)])
 
     # other stuff
     vg.add('exp_prefix', [EXP_PREFIX])
