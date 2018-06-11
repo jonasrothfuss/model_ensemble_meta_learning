a3de568b211fecbedde97bbbd6fedcb84e35458c
Jonas Rothfuss <jonas.rothfuss@gmx.de>
2018-06-10 18:58:25 -0700

    minor changes in bad-model plot

--- plots/bad-model-plots.py
+++ plots/bad-model-plots.py
@@ -19,19 +19,24 @@ def plot_from_exps(exp_data,
                    split_plots_by=None,
                    x_key='n_timesteps',
                    y_key=None,
-                   plot_name='./plot.png',
+                   plot_name='./bad-models.png',
                    subfigure_titles=None,
-                   plot_labels=None):
+                   plot_labels=None,
+                   x_label=None,
+                   y_label=None,
+                   fontsize=11):
 
     exp_data = filter(exp_data, filters=filters)
     exps_per_plot = group_by(exp_data, group_by_key=split_figures_by)
     fig, axarr = plt.subplots(1, len(exps_per_plot.keys()), figsize=(15, 4))
+    fig.tight_layout(pad=3.0, w_pad=4.0, h_pad=3.0, rect=[0, 0, 0.95, 1])
+
 
     # iterate over subfigures
     for i, (default_plot_title, plot_exps) in enumerate(sorted(exps_per_plot.items())):
         plots_in_figure_exps = group_by(plot_exps, split_plots_by)
         subfigure_title = subfigure_titles[i] if subfigure_titles else default_plot_title
-        axarr[i].set_title(subfigure_title, fontsize=10)
+        axarr[i].set_title(subfigure_title, fontsize=fontsize)
         axarr[i].xaxis.set_major_locator(plt.MaxNLocator(5))
 
         # iterate over plots in figure
@@ -43,7 +48,11 @@ def plot_from_exps(exp_data,
             axarr[i].plot(x, y_mean, label=label)
             axarr[i].fill_between(x, y_mean + y_std, y_mean - y_std, alpha=0.2)
 
-    fig.legend(loc='center right', ncol=1)
+            # axis labels
+            axarr[i].set_xlabel(x_label if x_label else x_key, fontsize=fontsize)
+            axarr[i].set_ylabel(y_label if y_label else y_key, fontsize=fontsize)
+
+    fig.legend(loc='center right', ncol=1, bbox_transform = plt.gcf().transFigure)
     fig.savefig(plot_name)
 
 
@@ -59,5 +68,7 @@ plot_from_exps(exps_data,
                subfigure_titles=['HalfCheetah - output_bias_range [0.0, 0.1]',
                                 'HalfCheetah - output_bias_range [0.0, 0.5]',
                                 'HalfCheetah - output_bias_range [0.0, 1.0]'],
-               plot_labels=['ME-MPG', 'ME-TRPO']
+               plot_labels=['ME-MPG', 'ME-TRPO'],
+               x_label='time steps',
+               y_label='average return',
                )
