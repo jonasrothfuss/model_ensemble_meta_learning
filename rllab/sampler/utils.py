d457f78b0c6fce3e7f0d61e88a6b01c62bcd6b15
Jonas Rothfuss <jonas.rothfuss@gmx.de>
2018-06-28 11:50:20 -0700

    simulating policies and render videos with correct timesteps

--- rllab/sampler/utils.py
+++ rllab/sampler/utils.py
@@ -3,13 +3,23 @@ from rllab.misc import tensor_utils
 import time
 
 
-def rollout(env, agent, max_path_length=np.inf, animated=False, speedup=1,
-            always_return_paths=False):
+def rollout(env, agent, max_path_length=np.inf, animated=False, speedup=1, save_video=True, video_filename='sim_out.mp4', ignore_done=False):
     observations = []
     actions = []
     rewards = []
     agent_infos = []
     env_infos = []
+    images = []
+
+    ''' get wrapped env '''
+    wrapped_env = env
+    while hasattr(wrapped_env, '_wrapped_env'):
+        wrapped_env = wrapped_env._wrapped_env
+
+    frame_skip = wrapped_env.frame_skip if hasattr(wrapped_env, 'frame_skip') else 1
+    assert hasattr(wrapped_env, 'dt'), 'environment must have dt attribute that specifies the timestep'
+    timestep = wrapped_env.dt
+
     o = env.reset()
     agent.reset()
     path_length = 0
@@ -24,15 +34,28 @@ def rollout(env, agent, max_path_length=np.inf, animated=False, speedup=1,
         agent_infos.append(agent_info)
         env_infos.append(env_info)
         path_length += 1
-        if d:
+        if d and not ignore_done: # and not animated:
             break
         o = next_o
         if animated:
             env.render()
-            timestep = 0.05
-            time.sleep(timestep / speedup)
-    if animated and not always_return_paths:
-        return
+            time.sleep(timestep*frame_skip / speedup)
+            if save_video:
+                from PIL import Image
+                image = env.wrapped_env.wrapped_env.get_viewer().get_image()
+                pil_image = Image.frombytes('RGB', (image[1], image[2]), image[0])
+                images.append(np.flipud(np.array(pil_image)))
+
+    if animated:
+        if save_video:
+            import moviepy.editor as mpy
+            fps = int(speedup/timestep * frame_skip)
+            clip = mpy.ImageSequenceClip(images, fps=fps)
+            if video_filename[-3:] == 'gif':
+                clip.write_gif(video_filename, fps=fps)
+            else:
+                clip.write_videofile(video_filename, fps=fps)
+        #return
 
     return dict(
         observations=tensor_utils.stack_tensor_list(observations),
--- scripts/sim_policy.py
+++ scripts/sim_policy.py
@@ -15,6 +15,12 @@ if __name__ == "__main__":
                         help='Max length of rollout')
     parser.add_argument('--speedup', type=float, default=1,
                         help='Speedup')
+    parser.add_argument('--video_filename', type=str,
+                        help='path to the out video file')
+    parser.add_argument('--prompt', type=bool, default=False,
+                        help='Whether or not to prompt for more sim')
+    parser.add_argument('--ignore_done', type=bool, default=True,
+                        help='Whether stop animation when environment done or continue anyway')
     args = parser.parse_args()
 
     # If the snapshot file use tensorflow, do:
@@ -26,7 +32,10 @@ if __name__ == "__main__":
         policy = data['policy']
         env = data['env']
         while True:
-            path = rollout(env, policy, max_path_length=args.max_path_length,
-                           animated=True, speedup=args.speedup)
-            if not query_yes_no('Continue simulation?'):
+            path = rollout(env, policy, max_path_length=args.max_path_length, animated=True, speedup=args.speedup,
+                           video_filename=args.video_filename, save_video=True, ignore_done=args.ignore_done)
+            if args.prompt:
+                if not query_yes_no('Continue simulation?'):
+                    break
+            else:
                 break
